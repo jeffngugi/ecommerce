f9245f7619d540cfb23b5ff494b2885261737c9f
jeffngugi <ngugimwathi254@gmail.com>
2020-05-12 15:44:43 +0300

    set up test with with jest and enzyme

---
+++ src/setupTests.js
@@ -0,0 +1,9 @@
+// import { configure } from 'enzyme';
+// import Enzyme from 'enzyme';
+// import * as Adapter from 'enzyme-adapter-react-16';
+
+// configure({ adapter: new Adapter() });
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
