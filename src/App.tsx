8c4b757beb1d28b0363980bb13f6b00baf7dd93d
jeffngugi <ngugimwathi254@gmail.com>
2020-06-10 11:03:47 +0300

    Integrated redux in the app for easy state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import Routes from "./components/Routes";
-
+import store from "./redux/store";
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes />
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Routes />
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
