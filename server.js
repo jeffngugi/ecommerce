7186dd4078b68c4c7d529f26ca6178e8dc9b5a64
jeffngugi <ngugimwathi254@gmail.com>
2020-07-08 22:47:20 +0300

    set up server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 let app = express();
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build", "index.html")));
 const port = process.env.PORT || "9000";
 app.set("port", port);
 const server = http.createServer(app);
